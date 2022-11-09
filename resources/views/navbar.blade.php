d7d443ed7655fe66ba7f3909accd1ccae6ccf348
OnlyJaximus <56784702+OnlyJaximus@users.noreply.github.com>
2022-11-09 09:09:33 -0800

    Update navbar.blade.php

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,7 +4,7 @@
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         {{-- <li class="{{ Request::path() == 'post/create' ? 'active' : ''}}"><a href="/post/create">New Post</a></li> --}}
         <li class="{{ Request::path() == 'users'? 'active':'' }}"><a href="/users">Users</a></li>
-        <li><a href="#">JavaScript</a></li>
+       {{--<li><a href="#">JavaScript</a></li>--}}
       </ul>
     </div>
   </nav>
